8a918a4c213f408f23f2a2fb3f193f9597074ceb
C.Jay Martin <cmartin@liaison-intl.com>
2013-02-08 12:14:52 -0500

    Added the container side (no tests yet)

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var jsdc_client = require('./lib/jsdc_client');
+var jsdc_container = require('./lib/jsdc_container');
 
-exports.jsdc_client = jsdc_client;
+
+//client expose
+exports.client = jsdc_client;
 exports.jsdc = jsdc_client.jsdc;
 exports.parse = jsdc_client.parse;
-exports.jsdc_connect = jsdc_client.connect;
+exports.connect = jsdc_client.connect;
 
+//container expose
+exports.container = jsdc_container;
+exports.interface = jsdc_container.interface;
+exports.listen = jsdc_container.listen
